0e3104d052d63df7211f1006501b0a87fd436ea5
VinkyV <75137786+VinkyV@users.noreply.github.com>
2026-06-25 19:12:50 +0300

    Implemented FixDolbyAtmos patch

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -2,12 +2,12 @@ group = "app.revanced"
 
 patches {
     about {
-        name = "ReVanced Patches template"
-        description = "Patches template for ReVanced"
-        source = "git@github.com:revanced/revanced-patches-template.git"
-        author = "ReVanced"
-        contact = "contact@revanced.app"
-        website = "https://revanced.app"
-        license = "GNU General Public License v3.0"
+        name = "Apple Music Patches"
+        description = "Patches for Apple Music"
+        source = "git@github.com:VinkyV/AppleMusicPatches.git"
+        author = "VinkyV"
+        contact = ""
+        website = ""
+        license = "GPL-3"
     }
 }
